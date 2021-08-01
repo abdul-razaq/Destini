2f2052950b408fb297c3b22fab5c80699c155710
AbdulRazaq Suleiman <razaqayomide01@gmail.com>
2021-08-01 20:35:39 +0100

    created initial UI

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,60 @@ class _StoryPageState extends State<StoryPage> {
     return Scaffold(
       body: Container(
         decoration: BoxDecoration(
-          image: DecorationImage(image: AssetImage(''), fit: BoxFit.cover),
+          image: DecorationImage(
+              image: AssetImage('images/background.png'), fit: BoxFit.cover),
+        ),
+        padding: EdgeInsets.symmetric(vertical: 50.0, horizontal: 15.0),
+        constraints: BoxConstraints.expand(),
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Expanded(
+                child: Center(
+                  child: Text(
+                    "Story text will go here...",
+                    style: TextStyle(fontSize: 25.0),
+                  ),
+                ),
+                flex: 12,
+              ),
+              Expanded(
+                  child: Container(
+                    child: TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        "Choice 1",
+                        style: TextStyle(
+                            fontSize: 25.0,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white),
+                      ),
+                    ),
+                    color: Colors.red,
+                  ),
+                  flex: 2,),
+              SizedBox(
+                height: 20.0,
+              ),
+              Expanded(
+                child: Container(
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      "Choice 2",
+                      style: TextStyle(
+                          fontSize: 25.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
+                    ),
+                  ),
+                  color: Colors.blue
+                ),
+                flex: 2,
+              ),
+            ],
+          ),
         ),
       ),
     );
